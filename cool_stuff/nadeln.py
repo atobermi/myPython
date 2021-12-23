57519e953351fb41de1656a5a76d23bf4a2f6a03
atobermi <63202883+atobermi@users.noreply.github.com>
2021-12-23 14:52:15 +0100

    Create nadeln.py

---
+++ cool_stuff/nadeln.py
@@ -0,0 +1,12 @@
+# prof. edmund weitz 
+# weihnachtsvorlesung 2021
+# https://www.youtube.com/watch?v=YIkqZsz4faA
+
+from random import unifrom
+from math import pi, cos, floor
+
+def buffon(n):
+  return sum(floor(uniform(0,1)+cos(unifrom(0,pi))) != 0
+    for _ in range(n) / n
+             
+buffon(1 000 000)             
